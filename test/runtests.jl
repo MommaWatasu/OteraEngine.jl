8beb4114f4499fbfdd1cfdd46eef4b633fdbdbd8
MommaWatasu <ascendwatson@gmail.com>
2023-05-04 15:57:41 +0900

    [test]update test
    add test for new functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,29 +3,36 @@ using Test
 
 @testset "OteraEngine.jl" begin
     #When initializing variables directly
-    @test_nowarn Template("test1.html")
-    tmp = Template("test1.html")
+    @test_nowarn Template("test1-1.html")
+    tmp = Template("test1-1.html")
     result = ""
-    open("test2.html", "r") do f
+    open("test1-2.html", "r") do f
         result = read(f, String)
     end
     @test result == tmp(jl_init=Dict("usr"=>"Julia"))
     
     #In case of no params and specified config
-    tmp = Template("test3.html", config=Dict("jl_block_start"=>"{{", "jl_block_stop"=>"}}"))
+    tmp = Template("test2.html", config=Dict("jl_block_start"=>"{{", "jl_block_stop"=>"}}"))
     regex = r"<strong>\s*(?<value>[0-9]*)\s*?</strong>"
     m = match(regex, tmp())
     @test 0 <= parse(Int, m[:value]) <= 100
     
     #include config file
-    @test_throws ArgumentError Template("test3.html", config_path="test.conf")
-    tmp = Template("test3.html", config_path="test.toml")
+    @test_throws ArgumentError Template("test2.html", config_path="test.conf")
+    tmp = Template("test2.html", config_path="test.toml")
     regex = r"<strong>\s*(?<value>[0-9]*)\s*?</strong>"
     m = match(regex, tmp())
     @test 0 <= parse(Int, m[:value]) <= 100
     
     #check `using` is available
-    tmp = Template("test4.html")
-    println(tmp())
+    tmp = Template("test3.html")
     @test_nowarn tmp()
+    
+    #check tmp codes work properly
+    tmp = Template("test4-1.html")
+    result = ""
+    open("test4-2.html", "r") do f
+        result = read(f, String)
+    end
+    @test result == tmp()
 end
