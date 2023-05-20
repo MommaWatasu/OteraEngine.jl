8fec1b2843f545b280c3ee6df1f04080aecdaa20
MommaWatasu <ascendwatson@gmail.com>
2023-05-20 21:14:31 +0900

    miner change in docs

--- src/template.jl
+++ src/template.jl
@@ -14,7 +14,7 @@ After you create a Template, you just have to execute the codes! For this, you u
 This is a simple usage:
 ```jldoctest
 julia> using OteraEngine
-julia> txt = "```using Dates; now();```. Hello {{ usr }}!"
+julia> txt = "```using Dates; now()```. Hello {{ usr }}!"
 julia> tmp = Template(html, path = false)
 julia> init = Dict("usr"=>"OteraEngine")
 julia> result = tmp(tmp_init = init)
