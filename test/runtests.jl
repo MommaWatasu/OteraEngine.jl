874faf9926f232a912770cd1f9668015f0451844
MommaWatasu <ascendwatson@gmail.com>
2023-11-07 20:47:44 +0900

    [test]add test for `dir` option and clean up tests
    add `dir` option

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,55 +2,58 @@ using OteraEngine
 using Test
 
 @testset "OteraEngine.jl" begin
-    # When initializing variables directly
-    @test_nowarn Template("test1-1.html")
-    tmp = Template("test1-1.html")
     result = ""
-    open("test1-2.html", "r") do f
+    # When initializing variables directly
+    @test_nowarn Template("jl_block1.html")
+    tmp = Template("jl_block1.html")
+    open("jl_block2.html", "r") do f
         result = read(f, String)
     end
     @test result == tmp(jl_init=Dict("usr"=>"Julia"))
     
     # In case of no params and specified config
-    tmp = Template("test2.html", config=Dict("jl_block"=>"@@@"))
+    tmp = Template("config.html", config=Dict("jl_block"=>"@@@"))
     regex = r"<strong>\s*(?<value>[0-9]*)\s*?</strong>"
     m = match(regex, tmp())
     @test 0 <= parse(Int, m[:value]) <= 100
     
     # include config file
-    @test_throws ArgumentError Template("test2.html", config_path="test.conf")
-    tmp = Template("test2.html", config_path="test.toml")
+    @test_throws ArgumentError Template("config.html", config_path="test.conf")
+    tmp = Template("config.html", config_path="test.toml")
     regex = r"<strong>\s*(?<value>[0-9]*)\s*?</strong>"
     m = match(regex, tmp())
     @test 0 <= parse(Int, m[:value]) <= 100
     
     # check `using` is available
-    tmp = Template("test3-1.html")
-    result = ""
-    open("test3-2.html", "r") do f
+    tmp = Template("jl_using1.html")
+    open("jl_using2.html", "r") do f
         result = read(f, String)
     end
     @test result == tmp()
     
     # check tmp codes work properly
-    tmp = Template("test4-1.html")
-    result = ""
-    open("test4-2.html", "r") do f
+    tmp = Template("tmp_block1.html")
+    open("tmp_block2.html", "r") do f
         result = read(f, String)
     end
     @test replace(result, "\r"=>"") == replace(tmp(), "\r"=>"")
 
-    # check `include` is available
-    tmp = Template("test5-1.html")
-    result = ""
-    open("test4-2.html", "r") do f
+    # check `include` and `extends` is available
+    tmp = Template("include_extends1.html")
+    open("include_extends4.html", "r") do f
         result = read(f, String)
     end
     @test replace(result, "\r"=>"") == replace(tmp(), "\r"=>"")
 
-    tmp = Template("test6-1.html")
-    result = ""
-    open("test6-4.html", "r") do f
+    # check if `dir` option is working
+    tmp = Template("wd_test/dir1.html")
+    open("wd_test/dir3.html", "r") do f
+        result = read(f, String)
+    end
+    @test replace(result, "\r"=>"") == replace(tmp(), "\r"=>"")
+    
+    tmp = Template("wd_test/dir1.html", config = Dict("dir"=>"wd_test"))
+    open("wd_test/dir3.html", "r") do f
         result = read(f, String)
     end
     @test replace(result, "\r"=>"") == replace(tmp(), "\r"=>"")
