e8020666a8f632737af1910cd11d443f048b1b74
MommaWatasu <ascendwatson@gmail.com>
2023-12-06 21:22:57 +0900

    [fix]fix macro
    macro was broken when the number of args was 1

--- src/macro.jl
+++ src/macro.jl
@@ -2,7 +2,7 @@ get_macro_name(macro_def::String) = match(r"(?<name>.*?)\(.*?\)", macro_def)[:na
 get_macro_args(macro_def::String) = match(r".*?(?<args>\(.*?\))", macro_def)[:args]
 apply_func(f, args...) = f(args...)
 
-function build_macro(macro_def::String, txt::String, filters::Dict{String, Function}, config::ParserConfig)
+function build_macro(macro_def::String, txt::String, filters::Dict{String, Symbol}, config::ParserConfig)
     out_txt = ""
     idx = 1
     for m in eachmatch(r"\{\{\s*(?<variable>.*?)\s*\}\}", txt)
@@ -28,13 +28,26 @@ function build_macro(macro_def::String, txt::String, filters::Dict{String, Funct
 end
 
 function apply_macros(txt::String, macros::Dict{String, String}, config::ParserConfig)
+    for (name, def) in zip(keys(macros), values(macros))
+        path = split(name, ".")
+        if length(path) != 1
+            name = path[end]
+        end
+        eval(Meta.parse(name*"="*def))
+    end
     re = Regex("$(config.expression_block[1])\\s*(?<name>.*?)(?<body>\\(.*?\\))\\s*$(config.expression_block[2])")
     m = match(re, txt)
     while !isnothing(m)
         split(m[:name], ".")[end] == "super" && break
         if haskey(macros, m[:name])
+            path = split(m[:name], ".")
+            if length(path) != 1
+                name = path[end]
+            else
+                name = m[:name]
+            end
             try
-                txt = txt[1:m.offset-1]*eval(Meta.parse("apply_func($(macros[m[:name]]), $(m[:body])...)"))*txt[m.offset+length(m.match):end]
+                txt = txt[1:m.offset-1]*eval(Meta.parse(name*m[:body]))*txt[m.offset+length(m.match):end]
             catch e
                 throw(ParserError("invalid macro: failed to call macro in $(m.match) because of the following error\n$e"))
             end
